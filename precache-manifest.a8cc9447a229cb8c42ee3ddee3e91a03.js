9bee564a5e58946466c9e31055683775fa9863ed
prabinspkt <prabin4998@gmail.com>
2020-01-30 16:37:34 -0800

    Updates

---
+++ precache-manifest.a8cc9447a229cb8c42ee3ddee3e91a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e713aca129378d04d982197cdbc3f9b7",
+    "url": "/Boggle-App/index.html"
+  },
+  {
+    "revision": "64198da8ba19f1b727ca",
+    "url": "/Boggle-App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "64198da8ba19f1b727ca",
+    "url": "/Boggle-App/static/js/2.0059a9b7.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Boggle-App/static/js/2.0059a9b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8016947e2a24dd984fdc",
+    "url": "/Boggle-App/static/js/main.bce28bf5.chunk.js"
+  },
+  {
+    "revision": "8884ffe318660f5438a8",
+    "url": "/Boggle-App/static/js/runtime-main.27b29e54.js"
+  }
+]);
